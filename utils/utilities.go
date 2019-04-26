067265904d5044780b29a409942a3dfb9b894d97
atighineanu <alexei.tighineanu@suse.com>
2019-04-26 13:09:04 +0200

    grinded Dehashinator

--- utils/utilities.go
+++ utils/utilities.go
@@ -2,9 +2,15 @@ package utils
 
 import (
 	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
@@ -22,6 +28,10 @@ func SetOSEnv(file string) (EnvOS, error) {
 	if err := json.NewDecoder(f).Decode(&auth); err != nil {
 		return nil, err
 	}
+	path, err := filepath.Abs(filepath.Dir(file))
+	if err != nil {
+		fmt.Printf("key.json or openstack.json path not properly set...%s", err)
+	}
 	env := EnvOS{
 		"OS_AUTH_URL=" + auth.AuthURL,
 		"OS_REGION_NAME=" + auth.RegionName,
@@ -30,7 +40,7 @@ func SetOSEnv(file string) (EnvOS, error) {
 		"OS_IDENTITY_API_VERSION=" + auth.IdentityAPIVersion,
 		"OS_INTERFACE=" + auth.Interface,
 		"OS_USERNAME=" + auth.Username,
-		"OS_PASSWORD=" + auth.Password,
+		"OS_PASSWORD=" + Dehashinator(path+"/../", path),
 		"OS_PROJECT_ID=" + auth.ProjectID,
 	}
 	return env, nil
@@ -96,20 +106,85 @@ func CmdRun(dir, openstackAPIauth, command string) (string, string) {
 	return outstd, outstderr
 }
 
-func Hashinator(s string, caasporsespdir string) {
-	h := md5.New()
-	h.Write([]byte(s))
-
-	f, err := ioutil.ReadFile(caasporsespdir + "/openstack.json")
+func Hashinator(pass string, homedir string, caasporsespdir string) {
+	v := OSAPI{}
+	var tempkey string
+	file, _ := os.Open(homedir + "/key.json")
+	decoder := json.NewDecoder(file)
+	defer file.Close()
+	err := decoder.Decode(&tempkey)
 	if err != nil {
-		fmt.Printf("Error at opening json file!...%s", err)
+		fmt.Println("This is bad! .json decoding didn't work:", err)
 	}
 
-	if strings.Contains(string(f), ":\"pass\",") || strings.Contains(string(f), "\"Password\":\"\",") {
-		f = []byte(strings.Replace(string(f), "pass", hex.EncodeToString(h.Sum(nil)), -1))
-		err = ioutil.WriteFile(caasporsespdir+"/openstack.json", f, 0644)
-		if err != nil {
-			fmt.Printf("Error at writing to json file!...%s", err)
-		}
+	file, _ = os.Open(homedir + caasporsespdir + "/openstack.json")
+	decoder = json.NewDecoder(file)
+	defer file.Close()
+	err = decoder.Decode(&v)
+	if err != nil {
+		fmt.Println("This is bad! .json decoding didn't work:", err)
 	}
+	block, _ := aes.NewCipher([]byte(Hasher(tempkey)))
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+	ciphertext := gcm.Seal(nonce, nonce, []byte(pass), nil)
+	f, err := ioutil.ReadFile(homedir + caasporsespdir + "/openstack.json")
+	if err != nil {
+		fmt.Printf("Error at opening json file!...%s")
+	}
+	v.Password = ciphertext[:]
+	f, _ = json.MarshalIndent(v, "", " ")
+	err = ioutil.WriteFile(homedir+caasporsespdir+"/openstack.json", f, 0644)
+	if err != nil {
+		fmt.Printf("Error at writing to json file!...%s")
+	}
+}
+
+func Dehashinator(homedir string, caasporsespdir string) string {
+
+	v := OSAPI{}
+	file, _ := os.Open(caasporsespdir + "/openstack.json")
+	decoder := json.NewDecoder(file)
+	defer file.Close()
+	err := decoder.Decode(&v)
+	if err != nil {
+		fmt.Println("This is bad! .json decoding didn't work:", err)
+	}
+
+	var tempkey string
+	file, _ = os.Open(homedir + "/key.json")
+	decoder = json.NewDecoder(file)
+	defer file.Close()
+	err = decoder.Decode(&tempkey)
+	if err != nil {
+		fmt.Println("This is bad! .json decoding didn't work:", err)
+	}
+
+	block, err := aes.NewCipher([]byte(Hasher(tempkey)))
+	if err != nil {
+		panic(err.Error())
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonceSize := gcm.NonceSize()
+	nonce, ciphertext := v.Password[:nonceSize], v.Password[nonceSize:]
+	decoded, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		panic(err.Error())
+	}
+	return string(decoded)
+}
+
+func Hasher(key string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(key))
+	return hex.EncodeToString(hasher.Sum(nil))
 }
