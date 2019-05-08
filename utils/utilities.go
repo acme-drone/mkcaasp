b8593a8e9199aa1e885fe0b886c7c484c8914dcb
atighineanu <alexei.tighineanu@suse.com>
2019-05-08 15:23:45 +0200

    grinded ostkcmd

--- utils/utilities.go
+++ utils/utilities.go
@@ -2,23 +2,16 @@ package utils
 
 import (
 	"bytes"
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/md5"
-	"crypto/rand"
-	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"html/template"
 	"io"
-	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strconv"
 	"strings"
-	"time"
 )
 
 var Cluster CaaSPCluster
@@ -42,10 +35,35 @@ func SetOSEnv(file string) (EnvOS, error) {
 		"OS_IDENTITY_API_VERSION=" + auth.IdentityAPIVersion,
 		"OS_INTERFACE=" + auth.Interface,
 		"OS_USERNAME=" + auth.Username,
-		"OS_PASSWORD=" + Dehashinator("./../", "./"),
+		"OS_PASSWORD=" + Dehashinator("./../", "./"), //Dehashinator("./../", "./"),  auth.Password
 		"OS_PROJECT_ID=" + auth.ProjectID,
 	}
 	return env, nil
+}
+
+func (s *CAASPOut) SSHCommand(cmd ...string) *exec.Cmd {
+	arg := append(
+		[]string{"-o", "StrictHostKeyChecking=no",
+			fmt.Sprintf("root@%s", s.IPAdminExt.Value),
+		},
+		cmd...,
+	)
+	return exec.Command("ssh", arg...)
+}
+
+func AdminOrchCmd(s *CAASPOut, option string) {
+	alias := []string{"docker", "exec", "$(docker ps -q --filter name=salt-master)", "salt", "-P", "\"roles:admin|kube-master|kube-minion\""}
+	//------------sweeping through options...
+	if option == "refresh" {
+		cmd := append(alias, "saltutil.refresh_grains")
+		out, err := s.SSHCommand(cmd...).CombinedOutput()
+		if err != nil {
+			log.Printf("ssh command didn't run as expected: %s\n", err)
+		}
+		fmt.Printf("%s", fmt.Sprintf("%s", string(out)))
+	}
+	//	withalias := append(
+	//		[]string{"docker", "exec", "$(docker ps -q --filter name=salt-master)", "salt", "-P", "\"roles:admin|kube-master|kube-minion\""}, cmd...)
 }
 
 func NodesAdder(dir string, append string, nodes *CAASPOut, Firstornot bool) *CaaSPCluster {
@@ -89,7 +107,7 @@ func NodesAdder(dir string, append string, nodes *CAASPOut, Firstornot bool) *Ca
 	}
 	templ, err := template.New("AddingNodes").Parse(CulsterTempl)
 	if err != nil {
-		log.Fatalf("Search didn't work...%s", err)
+		log.Fatalf("Error parsin ClusterTempl constant...%s", err)
 	}
 	var f *os.File
 	wd, _ := os.Getwd()
@@ -100,12 +118,12 @@ func NodesAdder(dir string, append string, nodes *CAASPOut, Firstornot bool) *Ca
 	}
 	err = templ.Execute(f, Cluster)
 	if err != nil {
-		fmt.Printf("%s", err)
+		log.Fatalf("couldn't execute the Cluster template %s", err)
 	}
 	f.Close()
 	out, err := exec.Command("cat", "terraform.tfvars").CombinedOutput()
 	if err != nil {
-		log.Printf("Error! %s\n", err)
+		log.Fatalf("Couldn't execute the command %s", err)
 	}
 	log.Printf("That's the modified cluster config:\n%s\n", fmt.Sprintf("%s", string(out)))
 	return &Cluster
@@ -163,7 +181,6 @@ func TfInit(dir string) {
 func CmdRun(dir, openstackAPIauth, command string) (string, string) {
 	wd, _ := os.Getwd()
 	os.Chdir(filepath.Join(wd, dir))
-	time.Sleep(10 * time.Second)
 	env, err := SetOSEnv(openstackAPIauth)
 	if err != nil {
 		log.Fatal(err)
@@ -172,145 +189,34 @@ func CmdRun(dir, openstackAPIauth, command string) (string, string) {
 	return outstd, outstderr
 }
 
-func Hashinator(pass string, homedir string, caasporsespdir string) {
-	v := OSAPI{}
-	var tempkey string
-	file, _ := os.Open(homedir + "/key.json")
-	decoder := json.NewDecoder(file)
-	defer file.Close()
-	err := decoder.Decode(&tempkey)
-	if err != nil {
-		fmt.Printf("This is bad! .json decoding didn't work at opening %s key.json:  %s\n", homedir, err)
-	}
-
-	file, _ = os.Open(homedir + "/" + caasporsespdir + "/openstack.json")
-	decoder = json.NewDecoder(file)
-	defer file.Close()
-	err = decoder.Decode(&v)
-	if err != nil {
-		fmt.Printf("This is bad! .json opening at %s openstack.json didn't work:  %s\n", homedir+"/"+caasporsespdir, err)
-	}
-	block, _ := aes.NewCipher([]byte(Hasher(tempkey)))
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		panic(err.Error())
-	}
-	nonce := make([]byte, gcm.NonceSize())
-	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		panic(err.Error())
-	}
-	ciphertext := gcm.Seal(nonce, nonce, []byte(pass), nil)
-	f, err := ioutil.ReadFile(homedir + "/" + caasporsespdir + "/openstack.json")
-	if err != nil {
-		fmt.Printf("Error at opening json file!...%s\n", err)
-	}
-	v.Password = ciphertext[:]
-	f, _ = json.MarshalIndent(v, "", " ")
-	err = ioutil.WriteFile(homedir+"/"+caasporsespdir+"/openstack.json", f, 0644)
-	if err != nil {
-		fmt.Printf("Error at writing to json file!...%s\n", err)
-	}
-}
-
-func Dehashinator(homedir string, caasporsespdir string) string {
-	v := OSAPI{}
-	file, _ := os.Open(caasporsespdir + "/openstack.json")
-	decoder := json.NewDecoder(file)
-	defer file.Close()
-	err := decoder.Decode(&v)
-	if err != nil {
-		fmt.Printf("This is bad! .json decoding didn't work @opening openstack.json: %s", err)
-	}
-
-	var tempkey string
-	file, _ = os.Open(homedir + "/key.json")
-	decoder = json.NewDecoder(file)
-	defer file.Close()
-	err = decoder.Decode(&tempkey)
-	if err != nil {
-		fmt.Printf("This is bad! .json decoding didn't work @opening key.json  %s:", err)
-	}
-
-	block, err := aes.NewCipher([]byte(Hasher(tempkey)))
-	if err != nil {
-		panic(err.Error())
-	}
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		panic(err.Error())
-	}
-	nonceSize := gcm.NonceSize()
-	nonce, ciphertext := v.Password[:nonceSize], v.Password[nonceSize:]
-	decoded, err := gcm.Open(nil, nonce, ciphertext, nil)
-	if err != nil {
-		panic(err.Error())
-	}
-	return string(decoded)
-}
-
-func Hasher(key string) string {
-	hasher := md5.New()
-	hasher.Write([]byte(key))
-	return hex.EncodeToString(hasher.Sum(nil))
-}
-
 func OpenstackCmd(dir string, openstackAPIauth string) (string, string) {
-	var stdoutBuf, stderrBuf bytes.Buffer
 	var a, b int
-	var command []string
 	hashtable := make(map[string]bool)
 	array := []string{"-repo", "-auth", "-action", "-createses", "-createcaasp", "-usage", "-sestfoutput", "-caasptfoutput", "-caaspuiinst", "-addnodes", "-nodes"}
 	for i := 0; i < len(array); i++ {
 		hashtable[array[i]] = true
 	}
+
 	for i := 0; i < len(os.Args); i++ {
 		if os.Args[i] == "-ostkcmd" {
-			a = i
+			a = i + 1
 		} else {
-			if a > 0 && i > a && hashtable[os.Args[i]] {
+			if !hashtable[os.Args[i]] {
 				b = i
 			}
 		}
 	}
-	command = append([]string{}, os.Args[a+1:b]...)
+
 	wd, _ := os.Getwd()
 	os.Chdir(filepath.Join(wd, dir))
 	env, err := SetOSEnv(openstackAPIauth)
 	if err != nil {
 		log.Fatal(err)
 	}
-	command = append([]string{"-c"}, command...)
-	cmd := exec.Command("bash", command...)
+	fmt.Println(os.Args[a : b+1])
 	newEnv := append(os.Environ(), env...)
+	cmd := exec.Command("openstack", os.Args[a:b+1]...)
 	cmd.Env = newEnv
-
-	stdoutIn, _ := cmd.StdoutPipe()
-	stderrIn, _ := cmd.StderrPipe()
-
-	var errStdout, errStderr error
-	stdout := io.MultiWriter(os.Stdout, &stdoutBuf)
-	stderr := io.MultiWriter(os.Stderr, &stderrBuf)
-
-	err = cmd.Start()
-	if err != nil {
-		log.Fatalf("cmd.Start() failed with '%s'\n", err)
-	}
-
-	go func() {
-		_, errStdout = io.Copy(stdout, stdoutIn)
-	}()
-
-	go func() {
-		_, errStderr = io.Copy(stderr, stderrIn)
-	}()
-
-	err = cmd.Wait()
-
-	if err != nil {
-		log.Fatalf("cmd.Run() failed with %s\n", err)
-	}
-	if errStdout != nil || errStderr != nil {
-		log.Fatal("failed to capture stdout or stderr\n")
-	}
-	return stdoutBuf.String(), stderrBuf.String()
+	out, err := cmd.CombinedOutput()
+	return fmt.Sprintf("%s", string(out)), fmt.Sprintf("%s", err)
 }
