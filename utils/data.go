24e0be3ce39f043fbdca318b2d500751c28458e6
atighineanu <alexei.tighineanu@suse.com>
2019-05-15 16:00:57 +0200

    corrected user.password to string type

--- utils/data.go
+++ utils/data.go
@@ -9,7 +9,7 @@ type OSAPI struct {
 	IdentityAPIVersion string
 	Interface          string
 	Username           string
-	Password           []byte
+	Password           string       //[]byte
 	ProjectID          string
 }
 
@@ -71,3 +71,5 @@ workers_vol_size = 5
 dnsdomain = "testing.qa.caasp.suse.net"
 dnsentry = 0
 stack_name = "INGSOC"`
+
+var 
