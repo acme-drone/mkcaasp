b0444be92d86e1a061eb10a8f3a62142bc57c9f0
atighineanu <alexei.tighineanu@suse.com>
2019-06-26 16:05:47 +0200

    added CheckSaltMinions in case orchestration update fails

--- utils/debug.go
+++ utils/debug.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"html/template"
 	"log"
+	"mkcaasp/utils"
 	"os"
 	"os/exec"
 	"strings"
@@ -72,5 +73,120 @@ func CheckVersions() {
 			}
 			break
 		}
+	}
+}
+
+func CheckSaltMinions() {
+	b := make(map[string]utils.SaltCluster)
+	var saltcluster utils.SaltCluster
+	args := []string{"-repo", "/home/atighineanu/work/CaaSP_kube/automation", "-auth", "openstack.json", "-cmd", "\"hostname\""}
+	out, err := exec.Command("caasp", args...).CombinedOutput()
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "running mkcaasp command failed: %s\n", err)
+	}
+	temp := strings.Split(fmt.Sprintf("%s", string(out)), "\n")
+	if strings.Contains(temp[0], "arning:") {
+		temp[0] = ""
+	}
+	for i := 0; i < len(temp)-1; i++ {
+		if strings.Contains(temp[i], ":") {
+			saltcluster.Name = strings.Replace(temp[i+1], " ", "", 1)
+			b[strings.Replace(temp[i], " ", "", 1)] = saltcluster
+		}
+		//	if temp[i] != "" && strings.Contains(temp[i], ":") {
+		//		fmt.Printf("b[%s]= %s\n", temp[i], b[temp[i]])
+		//	}
+	}
+
+	args = []string{"-repo", "/home/atighineanu/work/CaaSP_kube/automation", "-auth", "openstack.json", "-cmd", "\"cat /etc/salt/grains\""}
+	out, err = exec.Command("caasp", args...).CombinedOutput()
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "running mkcaasp command failed: %s\n", err)
+	}
+	temp = strings.Split(fmt.Sprintf("%s", string(out)), "\n")
+	if strings.Contains(temp[0], "arning:") {
+		temp[0] = ""
+	}
+	for i := 1; i < len(temp)-1; i++ {
+		if _, ok := b[strings.Replace(temp[i], " ", "", 1)]; ok {
+			saltcluster = b[strings.Replace(temp[i], " ", "", 1)]
+			for j := 1; j < len(temp)-i; j++ {
+				if _, ok := b[strings.Replace(temp[i+j], " ", "", 1)]; ok {
+					if strings.Contains(temp[i+j-1], "reboot_needed: true") {
+						saltcluster.RebootNeeded = true
+						b[strings.Replace(temp[i], " ", "", 1)] = saltcluster
+					}
+					break
+				}
+				if temp[i+j] == "" && strings.Contains(temp[i+j-1], "reboot_needed: true") {
+					saltcluster.RebootNeeded = true
+					b[strings.Replace(temp[i], " ", "", 1)] = saltcluster
+					break
+				}
+			}
+			//fmt.Println(b[strings.Replace(temp[i], " ", "", 1)])
+		}
+		//fmt.Printf("temp[%v] = %s\n", i, temp[i])
+		//if temp[i] != "" && strings.Contains(temp[i], ":") && !strings.Contains(temp[i], "reboot") && !strings.Contains(temp[i], "roles") && !strings.Contains(temp[i], "bootstrap") {
+		//	fmt.Printf("b[%s]= %s\n", temp[i], b[temp[i]])
+		//}
+	}
+
+	a := utils.CAASPOutReturner("openstack.json", "/home/atighineanu/work/CaaSP_kube/automation", "caasp-openstack-terraform")
+	var IPslicer []string
+	IPslicer = append(IPslicer, a.IPAdminExt.Value)
+	for _, k := range a.IPMastersExt.Value {
+		IPslicer = append(IPslicer, k)
+	}
+	for _, k := range a.IPWorkersExt.Value {
+		IPslicer = append(IPslicer, k)
+	}
+
+	for _, k := range IPslicer {
+		temp1 := ""
+		out, err := a.SSHCommand(k, "hostname").CombinedOutput()
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "running SSHCommand when debugging salt crashed... %s", err)
+		}
+		tmp := strings.Split(fmt.Sprintf("%s", string(out)), "\n")
+		for i := 0; i < len(tmp); i++ {
+			if strings.Contains(tmp[i], "caasp-") {
+				temp1 = tmp[i]
+			}
+		}
+		for key, value := range b {
+			if strings.Contains(value.Name, strings.Replace(temp1, " ", "", 1)) {
+				value.IP = k
+				b[key] = value
+			}
+		}
+	}
+
+	args = []string{"cat", "/etc/salt/grains"}
+	for _, value := range b {
+		out, err := value.SSHCmd(value.IP, args...).CombinedOutput()
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "running SSHCommand when debugging salt crashed... %s", err)
+		}
+		tmp := strings.Split(fmt.Sprintf("%s", string(out)), "\n")
+		c := 0
+		for _, k := range tmp {
+			fmt.Println(k)
+			if strings.Contains(k, "reboot_needed") {
+				c++
+			}
+		}
+		if c == 0 && value.RebootNeeded == false {
+			log.Println("This node needs to be updated!")
+			log.Printf("Updating...")
+
+			out := value.SSHCmd(value.IP, "/usr/sbin/transactional-update cleanup dup reboot")
+			utils.NiceBufRunner(out)
+		}
+	}
+
+	//---------------------final checker
+	for key, value := range b {
+		fmt.Printf("b[%s] = %s\n", key, value)
 	}
 }
