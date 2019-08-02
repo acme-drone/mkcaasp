1fd7f23b6ffabfda310cb4a66ffd905f5f8ea556
atighineanu <alexei.tighineanu@suse.com>
2019-08-02 18:40:58 +0200

    changed TFoutput logic according to .json changes []string to string on openstack load balancer

--- utils/datav4.go
+++ utils/datav4.go
@@ -1,11 +1,11 @@
 package utils
 
 type MKCaaSPCfg struct {
-	Platform  string  `json: "platform"`
-	Deploy    string  `json: "deploy"`
-	Vmware    *VMWare `json: "vmware"`
-	Skubaroot string  `json: "skubaroot"`
-	ClusterName string `json: "clustername"`
+	Platform    string  `json: "platform"`
+	Deploy      string  `json: "deploy"`
+	Vmware      *VMWare `json: "vmware"`
+	Skubaroot   string  `json: "skubaroot"`
+	ClusterName string  `json: "clustername"`
 }
 
 type VMWare struct {
@@ -25,19 +25,46 @@ type TFOutput struct {
 	IP_Workers       *TFTag `json: ip_workers`
 }
 
+type TFOutput_vmware struct {
+	IP_Load_Balancer *TFTag `json: ip_load_balancer`
+	IP_Masters       *TFTag `json: ip_masters`
+	IP_Workers       *TFTag `json: ip_workers`
+}
+
+type TFOutput_openstack struct {
+	IP_Load_Balancer *TFTagLoadBalancer `json: ip_load_balancer`
+	IP_Masters       *TFTag             `json: ip_masters`
+	IP_Workers       *TFTag             `json: ip_workers`
+}
+
+type TFTagLoadBalancer struct {
+	Sensitive bool   `json: sensitive`
+	Type      string `json: type`
+	Value     string `json: value`
+}
+
 type TFTag struct {
 	Sensitive bool     `json: sensitive`
 	Type      string   `json: type`
 	Value     []string `json: value`
 }
 
+type TFTag_variable struct {
+	Sensitive bool          `json: sensitive`
+	Type      string        `json: type`
+	Value     StringOrSlice `json: value`
+}
+
+type StringOrSlice interface{}
+
 //type ClusterCheck map[string]Node
 
 type SkubaCluster struct {
 	ClusterName string
-	Diagnosis map[string]Node
-	TF *TFOutput
-	Testdir string
+	Diagnosis   map[string]Node
+	TF_ostack   *TFOutput_openstack
+	TF_vmware   *TFOutput_vmware
+	Testdir     string
 }
 
 type Node struct {
